dd4ea7ca8fadf805ab8d8cdc6394f615aaf5f648
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-22 23:38:05 +0300

    Update EditTask.tsx

--- todolist/src/components/EditTask.tsx
+++ todolist/src/components/EditTask.tsx
@@ -2,7 +2,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 const EditTask = () => {
   return (
     <div className="text-white">
-      <div className="item"><span>This is the sample Task</span>
+      <div className="item ps-3"><span>This is the sample Task</span>
       <button className="bi-pencil btn text-white"></button>
       <button className="bi-trash btn text-white"></button>
       </div> 
