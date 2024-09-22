ddc4fbfffdaa4ec81978ca2d87cd7e685b0d71cc
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-22 22:46:12 +0300

    layout 50%,
    updated the add task input and the heading to h3 and most center

--- todolist/src/components/AddTask.tsx
+++ todolist/src/components/AddTask.tsx
@@ -2,8 +2,8 @@
 const AddTask = () => {
   return (
     <div>
-      <input type="text" name="addTask" id="addTask" placeholder="What is the task Today?" className="w-75" />
-      <button type="button" className="taskButton btn">Add Task</button>
+      <input type="text" name="addTask" id="addTask" placeholder="What is the task Today?" className="w-75 p-3" />
+      <button type="button" className="taskButton btn text-white">Add Task</button>
     </div>
   )
 }
--- todolist/src/components/Card.tsx
+++ todolist/src/components/Card.tsx
@@ -4,7 +4,7 @@ import AddTask from "./AddTask";
 const Card = () => {
   return (
     <div className="box">
-      <h1 className="text-center text-white">Get Things Done!</h1>
+      <h3 className="text-center text-white pt-3 pb-4">Get Things Done!</h3>
       <AddTask />
     </div>
   );
