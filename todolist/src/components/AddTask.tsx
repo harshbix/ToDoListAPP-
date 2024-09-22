610b445578130efb17dae5feb587eae86c01cb8e
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-22 22:39:22 +0300

    fixed the button

--- todolist/src/components/AddTask.tsx
+++ todolist/src/components/AddTask.tsx
@@ -2,7 +2,8 @@
 const AddTask = () => {
   return (
     <div>
-      <input type="text" name="addTask" id="addTask" placeholder="What is the task Today?" />
+      <input type="text" name="addTask" id="addTask" placeholder="What is the task Today?" className="w-75" />
+      <button type="button" className="taskButton btn">Add Task</button>
     </div>
   )
 }
--- todolist/src/components/Card.tsx
+++ todolist/src/components/Card.tsx
@@ -3,7 +3,7 @@ import AddTask from "./AddTask";
 
 const Card = () => {
   return (
-    <div className="bg-primary box">
+    <div className="box">
       <h1 className="text-center text-white">Get Things Done!</h1>
       <AddTask />
     </div>
